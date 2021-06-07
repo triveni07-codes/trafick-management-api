4e50da3ff8c75f5a535de8a9e3fbc7edd58fef9c
Triveni <triveni.wadkar@gmail.com>
2021-06-07 20:12:37 +0200

    Initial commit

---
+++ src/test/java/com/assignment/trafickmanagementapi/TrafickManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.trafickmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafickManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
