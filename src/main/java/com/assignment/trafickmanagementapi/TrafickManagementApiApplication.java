6148c0b91d054877d5a286048ebe6e53fa599f84
Triveni <triveni.wadkar@gmail.com>
2021-06-07 20:58:53 +0200

    Add swagger config

--- src/main/java/com/assignment/trafickmanagementapi/TrafickManagementApiApplication.java
+++ src/main/java/com/assignment/trafickmanagementapi/TrafickManagementApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrafickManagementApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrafickManagementApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TrafickManagementApiApplication.class, args);
+  }
 
 }
